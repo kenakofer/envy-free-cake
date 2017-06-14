a861e5d6665ad05c04c1afb2b1782e3ffcc2f61f
Kenan Clifford Bitikofer <kcbitikofer@un004-imini26.local>
2017-06-14 11:12:25 -0400

    Add very bad (worst?) cases to data.out

--- empirical_analysis.py
+++ empirical_analysis.py
@@ -6,14 +6,16 @@ OUTFILE = './data.out'
 def core_random(agent_number):
     divisions = 10
     agents = [Agent(division_count=divisions) for i in range(agent_number)]
-    cake = Cake()
+    write_scenario_to_file(agents, OUTFILE)
 
+    
+
+def write_scenario_to_file(agents, filename):
     info_line = ""
     for a in agents:
         info_line += a.get_preference_string() + '; '
-
     try:
-        core(agents[0], agents, cake.pieces[0])
+        core(agents[0], agents, Piece.get_whole_piece())
     except AssertionError:
         print("We hit a False Assertion! Here is the agent data to reproduce:")
         print(info_line)
@@ -24,10 +26,21 @@ def core_random(agent_number):
     info_line += '| '
     info_line += str(trim_count) + ' | '
     info_line += str(value_count)
-    with open(OUTFILE, 'a') as f:
+    with open(filename, 'a') as f:
         print(info_line, file=f)
 
 
+def core_worst_case():
+        debug_print("Testing a possible worst case call to prints")
+        for n in range(4,12):
+            print(n)
+            divs = 30
+            agents = [
+                Agent(division_count=divs, preference_function=lambda x: x**i) for i in range(1, n+1)
+            ]
+            write_scenario_to_file(agents, OUTFILE)
+            #print("sum:",trim_count+value_count)
+            #self.assertTrue( value_count + trim_count <= worst_cases_for_n_players[n] )
 
 
 if __name__ == '__main__':

c219877bd10ae3cba98ac22c6e92d28a8019f3e1
Kenan Clifford Bitikofer <kcbitikofer@un004-imini26.local>
2017-06-22 18:57:20 -0400

    Add debug_print prefixes

--- debug.py
+++ debug.py
@@ -1,14 +1,20 @@
 debug=False
+prefix = ""
 
 def debug_print(*obj):
     if (debug):
         if len(obj) == 0:
             print()
         else:
-            print(' '.join(map(str, obj)))
+            print(prefix,' '.join(map(str, obj)))
 
 def set_debug(value):
     global debug
     debug=value
+
+def set_debug_prefix(string):
+    global prefix
+    prefix=string
+
             
 

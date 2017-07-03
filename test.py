8acc0fa8f3af60c8d1544236c1f7279355c51d4a
Kenan Bitikofer <kbitikofer@gmail.com>
2017-07-03 17:25:54 -0400

    Seed issues to fix

---
+++ test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from agent import *
+from piece import *
+from core import *
+import random
+from envy_free_allocation import *
+from copy import copy
+from time import time
+
+if __name__ == '__main__':
+    set_debug(True)
+    #seed = 1498158526370
+    #seed = 1498176799854
+    #seed = 1498579359179
+    
+    ## Breaks aziz and mackenzie tiebreaking 
+    #seed = 1499099201146
+
+    ## Breaks global ranking tiebreaking
+    #seed = 1499109752540
+
+    ## Breaks ranking plus agent ordering
+    ## But it doesn't actually break. Why?
+    seed =  1499115613878
+
+    random.seed(seed)
+    agents = [Agent(division_count=random.randint(10,20)) for i in range(12)]
+    core_number = get_envy_free_allocation(agents, Piece.get_whole_piece(), get_call_number=True)

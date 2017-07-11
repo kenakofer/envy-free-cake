45d1c901733d9ba39f2f8c170d382fbd0be063bc
Kenan Clifford Bitikofer <kcbitikofer@un004-imini26.local>
2017-07-11 13:17:15 -0400

    Further additions

--- waste_makes_haste.py
+++ waste_makes_haste.py
@@ -25,7 +25,49 @@ Raphael Reitzig and Sebastian Wild, April 13, 2015
 https://arxiv.org/pdf/1502.04048.pdf
 '''
 def select_l_star(L, k):
-    #TODO
+    n = len(L)
+    ## 1
+    np = min(k, n+1)
+    ## 2
+    if np <= n:
+        ## 2.1 Determine L_co := L(n'), i. e. the n'th largest length
+        L_co = sorted(L, reverse=True)[np-1]
+        ## 2.2. If L_co is optimal, answer l* = L_co (and terminate).
+        if is_optimal(L, k, L_co):
+            return L_co
+    ## 2' so np > n
+    else:
+        ## 2.4
+        L_co = 0
+    ## 3 Assemble I_co = I_>L_co
+    ## pg 14: We will restrict ourselves for the remainder of this 
+    ## article to index sets I_co that contain indices of lengths that 
+    ## are larger than the n'th largest length L(n') in L
+    I_co = [i for i,stick in enumerate(L) if stick > L_co]
+    ## 4 Compute C := C(I_co, f_l, f_u) as multiset.
+    ## pg 13 gives definition of candidate multiset
+
+
+'''
+(pg 17), step 2.2 in the algorithm proof
+'''
+def is_optimal(L, k, l):
+    a, m_L = m(L, l)
+    return k <= m_L < k+a 
+
+'''
+The number of pieces of length l that we can get by cutting the set 
+of sticks L into sticks no longer than l. (pg 9) The computation of
+a is added as per step 2.2 on pg 17
+'''
+def m(L, length):
+    a = 0
+    m = 0
+    for stick in L:
+        m += stick // length
+        if stick % length == 0:
+            a+=1
+    return a, m
 
 def P(k):
     return 2**(k-1) + 1
